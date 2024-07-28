a7f7fbbeeaa27c3862253dfa79a20a0074eda098
Adrian <adrianhakimutomo@gmail.com>
2024-07-28 11:17:15 +0700

    add button

--- src/components/ChatDrawer.tsx
+++ src/components/ChatDrawer.tsx
@@ -26,6 +26,7 @@ import {
   useRive,
   useStateMachineInput,
 } from "@rive-app/react-canvas";
+import Quiz from "./Quiz";
 import RecommendationCarousel from "./RecommendationCarousel";
 
 export default function ChatDrawer() {
@@ -45,6 +46,8 @@ export default function ChatDrawer() {
   const [messages, setMessages] = useState<{ content: any; isSelf: boolean }[]>(
     [],
   );
+
+  const [inputType, setInputType] = useState("");
 
   const openInput = useStateMachineInput(rive, "State Machine 1", "open_trig");
   const bingInput = useStateMachineInput(rive, "State Machine 1", "bing_trig");
@@ -96,95 +99,106 @@ export default function ChatDrawer() {
         <DrawerHeader>
           <DrawerTitle className="bg-blend-color-burn">GrabFood AI</DrawerTitle>
         </DrawerHeader>
-        <div className="relative flex grow flex-col">
-          <div
-            className={cn(
-              "pointer-events-none absolute inset-0 left-1/2 top-1/4 -z-10 flex w-full grow -translate-x-1/2 -translate-y-1/2 transition-transform duration-700",
-              messages.length > 0 && "-translate-y-[95%]",
-            )}
-          >
-            <RiveComponent />
+        {inputType === "" && (
+          <div className="relative flex grow flex-col">
+            <Button onClick={() => setInputType("text")}>Text</Button>
+            <Button onClick={() => setInputType("quiz")}>Quiz</Button>
           </div>
-          <ScrollArea
-            className={cn(
-              "flex h-1 grow flex-col justify-end gap-2 transition-transform pb-28",
-              messages.length > 0 && "translate-y-20",
-            )}
-          >
-            <motion.div
-              className="flex justify-start gap-2 px-5 py-2"
-              layout
-              initial={{ opacity: 0, scale: 0.9 }}
-              animate={{ opacity: 1, scale: 1 }}
+        )}
+        {inputType === "quiz" && <Quiz></Quiz>}
+        {inputType === "text" && (
+          <div className="relative flex grow flex-col">
+            <div
+              className={cn(
+                "pointer-events-none absolute inset-0 left-1/2 top-1/4 -z-10 flex w-full grow -translate-x-1/2 -translate-y-1/2 transition-transform duration-700",
+                messages.length > 0 && "-translate-y-[95%]",
+              )}
             >
-              <div className="max-w-[80%] origin-top-left rounded-2xl rounded-tl-md border border-gray-300 px-4 py-2 text-gray-900">
-                Hi! I&apos;m your GrabFood AI Assitant. How can I help you find
-                the perfect meal today?
-              </div>
-            </motion.div>
-            {messages.map((message, index) => (
+              <RiveComponent />
+            </div>
+            <ScrollArea
+              className={cn(
+                "flex h-1 grow flex-col justify-end gap-2 pb-28 transition-transform",
+                messages.length > 0 && "translate-y-20",
+              )}
+            >
               <motion.div
-                key={index}
-                ref={index + 1 === messages.length ? messageRef : null}
-                className={cn(
-                  "flex w-screen gap-2 px-5 py-2",
-                  message.isSelf ? "justify-end" : "justify-start",
-                )}
+                className="flex justify-start gap-2 px-5 py-2"
                 layout
                 initial={{ opacity: 0, scale: 0.9 }}
                 animate={{ opacity: 1, scale: 1 }}
               >
-                <div
-                  className={cn("max-w-[80vw] rounded-2xl px-4 py-2", {
-                    "origin-top-right self-end rounded-tr-md border border-green-700 bg-gradient-to-br from-green-500 to-green-600 text-white":
-                      message.isSelf,
-                    "origin-top-left rounded-tl-md border border-gray-300 text-gray-900":
-                      !message.isSelf,
-                    "mb-[180px]":
-                      !message.isSelf &&
-                      typeof message.content === "object" &&
-                      message.content !== null,
-                  })}
-                >
-                  {typeof message.content === "object" &&
-                  message.content !== null ? (
-                    <RecommendationCarousel recommendations={message.content} />
-                  ) : (
-                    message.content
-                  )}
+                <div className="max-w-[80%] origin-top-left rounded-2xl rounded-tl-md border border-gray-300 px-4 py-2 text-gray-900">
+                  Hi! I&apos;m your GrabFood AI Assitant. How can I help you
+                  find the perfect meal today?
                 </div>
               </motion.div>
-            ))}
-            <ScrollBar />
-          </ScrollArea>
-          <div className="z-10 px-5">
-            <form action={handleSubmit} className="flex gap-2">
-              <Input
-                ref={inputRef}
-                id="message"
-                name="message"
-                required
-                autoComplete="off"
-                placeholder={
-                  messages.length === 0
-                    ? "Did you already have something in mind?"
-                    : "Ask again..."
-                }
-                onFocus={handleInputFocus}
-                onBlur={handleInputBlur}
-              />
-              <Button
-                type="submit"
-                variant="ghost"
-                size="icon"
-                className="transition-transform active:scale-90"
-                onClick={() => bingInput?.fire()}
-              >
-                <i className="i-solar-plain-bold size-6 text-gray-900/70" />
-              </Button>
-            </form>
+              {messages.map((message, index) => (
+                <motion.div
+                  key={index}
+                  ref={index + 1 === messages.length ? messageRef : null}
+                  className={cn(
+                    "flex w-screen gap-2 px-5 py-2",
+                    message.isSelf ? "justify-end" : "justify-start",
+                  )}
+                  layout
+                  initial={{ opacity: 0, scale: 0.9 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                >
+                  <div
+                    className={cn("max-w-[80vw] rounded-2xl px-4 py-2", {
+                      "origin-top-right self-end rounded-tr-md border border-green-700 bg-gradient-to-br from-green-500 to-green-600 text-white":
+                        message.isSelf,
+                      "origin-top-left rounded-tl-md border border-gray-300 text-gray-900":
+                        !message.isSelf,
+                      "mb-[180px]":
+                        !message.isSelf &&
+                        typeof message.content === "object" &&
+                        message.content !== null,
+                    })}
+                  >
+                    {typeof message.content === "object" &&
+                    message.content !== null ? (
+                      <RecommendationCarousel
+                        recommendations={message.content}
+                      />
+                    ) : (
+                      message.content
+                    )}
+                  </div>
+                </motion.div>
+              ))}
+              <ScrollBar />
+            </ScrollArea>
+            <div className="z-10 px-5">
+              <form action={handleSubmit} className="flex gap-2">
+                <Input
+                  ref={inputRef}
+                  id="message"
+                  name="message"
+                  required
+                  autoComplete="off"
+                  placeholder={
+                    messages.length === 0
+                      ? "Did you already have something in mind?"
+                      : "Ask again..."
+                  }
+                  onFocus={handleInputFocus}
+                  onBlur={handleInputBlur}
+                />
+                <Button
+                  type="submit"
+                  variant="ghost"
+                  size="icon"
+                  className="transition-transform active:scale-90"
+                  onClick={() => bingInput?.fire()}
+                >
+                  <i className="i-solar-plain-bold size-6 text-gray-900/70" />
+                </Button>
+              </form>
+            </div>
           </div>
-        </div>
+        )}
         <DrawerFooter>
           <DrawerClose>
             <Button
